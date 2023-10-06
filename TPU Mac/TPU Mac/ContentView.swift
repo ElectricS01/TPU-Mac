a06b3edabae0b67c4257d275563dc6895d6a8251
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-10-06 21:16:30 +1100

    Sidebar

--- TPU Mac/TPU Mac/ContentView.swift
+++ TPU Mac/TPU Mac/ContentView.swift
@@ -7,41 +7,69 @@
 
 import SwiftUI
 
-struct MenuItem: Identifiable, Hashable {
-    let name: String
-    let id = UUID()
-}
-
-
-private var oceans = [
-    MenuItem(name: "Home"),
-    MenuItem(name: "Settings"),
-    MenuItem(name: "Gallery"),
-    MenuItem(name: "Comms"),
-    MenuItem(name: "Skill issue")
-]
-
 struct TwoColumnSplitView: View {
-    
-    @State private var selectedCategoryId: MenuItem.ID?
     
     var body: some View {
         NavigationSplitView {
-            List(oceans) { ocean in
-                HStack {
-                    
-                    Text(ocean.name)
-                        .font(.system(.title3, design: .rounded))
-                        .bold()
+            List {
+                NavigationLink(destination: HomeView()) {
+                    Label("Home", systemImage: "house")
+                }
+                NavigationLink(destination: SettingsView()) {
+                    Label("Settings", systemImage: "gear")
+                }
+                NavigationLink(destination: GalleryView()) {
+                    Label("Gallery", systemImage: "photo.on.rectangle")
+                }
+                NavigationLink(destination: CommsView()) {
+                    Label("Comms", systemImage: "message")
+                }
+                NavigationLink(destination: AboutView()) {
+                    Label("About", systemImage: "info.circle")
                 }
             }
         } detail: {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("BetterTPU tbh")
+            Text("TPU Mac")
             
         }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+            .navigationTitle("Home")
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+            .navigationTitle("Settings")
+    }
+}
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+            .navigationTitle("Gallery")
+    }
+}
+
+struct CommsView: View {
+    var body: some View {
+        Text("Comms")
+            .navigationTitle("Comms")
+    }
+}
+
+struct AboutView: View {
+    var body: some View {
+        Text("About")
+            .navigationTitle("About")
     }
 }
 
